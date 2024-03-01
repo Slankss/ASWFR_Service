70615bf4903ab624267f4a919d401746a3e2cf9b
okankkl <okankukull@gmail.com>
2024-03-01 10:25:52 +0300

    base64 to image and image to base64 functions done

---
+++ venv/main/ImageProcess.py
@@ -0,0 +1,21 @@
+from Constant import image_base64
+import pybase64
+import base64
+
+
+def base64_to_image():
+    decodedData = pybase64.b64decode(image_base64)
+    image = open("decodedImage.jpeg", "wb")
+    image.write(decodedData)
+    image.close()
+
+
+def image_to_base64():
+
+    image = open("originalImage.jpeg", "rb")
+    image_bytes = pybase64.b64encode(image.read())
+    base64 = image_bytes.decode()
+    print(base64)
+
+
+image_to_base64()
