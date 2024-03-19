5afbfd7bcd11ce3306b267f4e7a317e708f06203
okankkl <okankukull@gmail.com>
2024-03-19 14:46:21 +0300

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn venv/acwfr:app
+web: gunicorn venv acwfr:app
--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from venv import *
-
-app = Flask(__name__)
-if __name__ == "__main__":
-
-    app.run(debug=True)
